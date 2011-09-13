1ac33c6052438be37736580e96cee3289a622b61
Alex Gaynor <alex.gaynor@gmail.com>
2011-09-13 16:40:01 -0400

    started stubbing out models.

--- rooms_project/rooms/models.py
+++ rooms_project/rooms/models.py
@@ -3,3 +3,18 @@ from django.db import models
 
 class Building(models.Model):
     name = models.CharField(max_length=255)
+
+class Floor(models.Model):
+    building = models.ForeignKey(Building, related_name="floors")
+    name = models.CharField(
+        max_length=100,
+        help_text="For most buildings, the floors are just named by their number",
+    )
+
+class Room(models.Model):
+    floor = models.ForeignKey(Floor, related_name="rooms")
+
+    name = models.CharField(
+        max_length=100,
+        help_text="The name of the room, for many rooms this is just the number.",
+    )
