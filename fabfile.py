9b5e64c2eb29a6e9603ad99c34d3c4ffce07f690
Alex Gaynor <alex.gaynor@gmail.com>
2011-09-17 17:00:58 -0400

    Added a fabfile to control deployment, and now include the built docs in the site.

---
+++ fabfile.py
@@ -0,0 +1,9 @@
+from fabric.api import local
+from fabric.context_managers import lcd
+
+
+def deploy():
+    local("python rooms_project/manage.py collectstatic --noinput")
+    with lcd("docs"):
+        local("make html")
+    local("epio upload")
