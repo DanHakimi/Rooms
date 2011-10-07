7319c179f9969bd6025ae52d58e82d43ea0a9b6b
Mark Dube <mark@doob-un4.(none)>
2011-10-07 16:52:15 -0400

    fix

--- rooms_project/cas_auth/urls.py
+++ rooms_project/cas_auth/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = patterns("",
-    url(r"login/$", views.login, name="auth_login"),
-    url(r"logout/$", views.logout, name="auth_logout"),
+    url(r"^login/$", views.login, name="auth_login"),
+    url(r"^logout/$", views.logout, name="auth_logout"),
 )
