a245e826545d04bccfa167b8ad9c32c001479b91
Mark Dube <mark@doob-un4.(none)>
2011-10-16 17:24:39 -0400

    fixture

--- rooms_project/rooms/fixture_gen.py
+++ rooms_project/rooms/fixture_gen.py
@@ -2,10 +2,11 @@ from fixture_generator import fixture_generator
 
 from .models import Building, Floor, Room
 
-
 @fixture_generator(Building, Floor, Room)
 def rpi_union():
     union = Building.objects.create(name="The Union")
-    union.floors.create(name="1st")
-    union.floors.create(name="2nd")
-    union.floors.create(name="3rd")
+    first_floor = union.floors.create(name="1st")
+    second_floor = union.floors.create(name="2nd")
+    third_floor = union.floors.create(name="3rd")
+		first_floor.rooms.create(capacity=100, name="Some Room", nickname="cool room")
+		
