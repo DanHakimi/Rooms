8d0c8493035a626dbf9611e1d77232ceee61eeab
Mark Dube <mark@doob-un4.(none)>
2011-10-24 19:48:04 -0400

    impl

--- static/js/rooms.js
+++ static/js/rooms.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
 	$.getJSON("/api/v1/building/",function(buildings_data){
 		for (var i=0; i < buildings_data.length; i++){
-			$("<div class='building'></div>")
-				.appendTo("#container")
-				.append($("<h2 id='b"+buildings_data[i].id+"'>"+buildings_data[i].name+"</h2>").click(function(){
-						$.getJSON("/api/v1/building/"+$(this).attr('id').substring(1)+"/",function(building_data){
-							for (var i=0; i < building_data.floors.length; i++){
-								$("<div class='floor' id='b"+building_data.id+"f"+building_data.floors[i].id+"'>"+building_data.floors[i].name+"</div>").click(function(){
-									$.getJSON("/api/v1/building/"+$(this).attr('id').substring(1).split('f')[0]+"/floor/"+$(this).attr('id').split('f')[1]+"/",function(floor_data){
+			$("<div class='building' id='b"+buildings_data[i].id+"'><h2>"+buildings_data[i].name+"</h2><div class='floors'></div></div>").click(function(){
+				if ($(".floors",$(this)).children().length==0){
+					$.getJSON("/api/v1/building/"+buildings_data[i].id+"/",function(building_data){
+						for (var i=0; i < building_data.floors.length; i++){
+							$("<div class='floor' id='b"+building_data.id+"f"+building_data.floors[i].id+"'><h3>"+building_data.floors[i].name+"</h3><div class='rooms'></div></div>").click(function(){
+								if ($(".rooms",$(this)).children().length==0){
+									$.getJSON("/api/v1/building/"+building_data.id+"/floor/"+building_data.floors[i].id+"/",function(floor_data){
 										for (var i=0; i<floor_data.rooms.length; i++){
-											$("<div class='room'><h3>"+floor_data.rooms[i].name+"</h3><p>Capactiy: "+floor_data.rooms[i].capacity+"</p></div>").appendTo($("#b"+building_data.id+"f"+floor_data.id));
+											$("<div class='room' id='b"+building_data.id+"f"+floor_data.id+"r"+floor_data.floors[i].id+"'><h4>"+floor_data.rooms[i].name+"</h4><div class='details'><p>Capactiy: "+floor_data.rooms[i].capacity+"</p></div></div>").hide().appendTo("#b"+building_data.id+"f"+floor_data.id+" .rooms");
 										}
 									});
-								}).appendTo($("#b"+building_data.id).parent());
-							}
-						});
-					})
-				).append("<img src='http://placehold.it/250x150'>");
+								}
+								$("#b"+building_data.id+"f"+floor_data.id+" .rooms").toggle(500);
+							}).hide().appendTo("#b"+building_data.id+" .floors");
+						}
+					});
+				}
+				$("#b"+building_data.id+" .floors").toggle(500);
+			}).appendTo("#buildings");
 		}
 	});
-});
+});	
