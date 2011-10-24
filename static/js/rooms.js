a9f46036f33edc8da9519186b390a0c996cde7cd
Mark Dube <mark@doob-un4.(none)>
2011-10-24 17:48:30 -0400

    implemenation

--- static/js/rooms.js
+++ static/js/rooms.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
-	$.getJSON("http://campus-rooms.ep.io/api/v1/building/",function(data){
-		for (var i=0; i < data.length; i++){
+	$.getJSON("/api/v1/building/",function(buildings_data){
+		for (var i=0; i < buildings_data.length; i++){
 			$("<div class='building'></div>")
 				.appendTo("#container")
-				.append("<img src='http://placehold.it/250x150'>")
-				.append($("<h2 id='b"+data[i]["id"]+"'>"+data[i]["name"]+"</h2>").click(function(){
-						$.getJSON("http://campus-rooms.ep.io/api/v1/building/"+$(this).attr('id')+"/",function(data){
-							for (var i=0; i < data["floors"].length; i++){
-								$("<div class='floor' id='f"+data[i]["id"]+"'>"+data["floors"][i].name+"</div>").click(function(){
-									
-								});
+				.append($("<h2 id='b"+buildings_data[i].id+"'>"+buildings_data[i].name+"</h2>").click(function(){
+						$.getJSON("/api/v1/building/"+$(this).attr('id').substring(1)+"/",function(building_data){
+							for (var i=0; i < building_data.floors.length; i++){
+								$("<div class='floor' id='b"+building_data.id+"f"+building_data.floors[i].id+"'>"+building_data.floors[i].name+"</div>").click(function(){
+									$.getJSON("/api/v1/building/"+$(this).attr('id').substring(1).split('f')[0]+"/floor/"+$(this).attr('id').split('f')[1],function(floor_data){
+										
+									});
+								}).appendTo($(this));
 							}
 						}).appendTo($(this));
 					})
-				);
+				)
+				.append("<img src='http://placehold.it/250x150'>");
 		}
 	});
 });
