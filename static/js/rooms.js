50e1cdeabe43fc81e7bb6e78f3bccbb3b33fb8cb
Mark Dube <mark@doob-un4.(none)>
2011-10-24 17:37:47 -0400

    implemenation

--- static/js/rooms.js
+++ static/js/rooms.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 				.appendTo("#container")
 				.append("<img src='http://placehold.it/250x150'>")
 				.append($("<h2 id='b"+data[i]["id"]+"'>"+data[i]["name"]+"</h2>").click(function(){
-						$.getJSON("http://campus-rooms.ep.io/api/v1/building/"+data[i]["id"]+"/",function(data){
+						$.getJSON("http://campus-rooms.ep.io/api/v1/building/"+$(this).attr('id')+"/",function(data){
 							for (var i=0; i < data["floors"].length; i++){
 								$("<div class='floor' id='f"+data[i]["id"]+"'>"+data["floors"][i].name+"</div>").click(function(){
 									
