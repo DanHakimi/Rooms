039fcc50abfce3a9b4e331751b09e45bf4cbe9a4
Mark Dube <mark@doob-un4.(none)>
2011-10-24 18:46:27 -0400

    impl

--- static/js/rooms.js
+++ static/js/rooms.js
@@ -7,15 +7,16 @@ $(document).ready(function(){
 						$.getJSON("/api/v1/building/"+$(this).attr('id').substring(1)+"/",function(building_data){
 							for (var i=0; i < building_data.floors.length; i++){
 								$("<div class='floor' id='b"+building_data.id+"f"+building_data.floors[i].id+"'>"+building_data.floors[i].name+"</div>").click(function(){
-									$.getJSON("/api/v1/building/"+$(this).attr('id').substring(1).split('f')[0]+"/floor/"+$(this).attr('id').split('f')[1],function(floor_data){
-										
+									$.getJSON("/api/v1/building/"+$(this).attr('id').substring(1).split('f')[0]+"/floor/"+$(this).attr('id').split('f')[1]+"/",function(floor_data){
+										for (var i=0; i<floor_data.rooms.length; i++){
+											$("<div class='room'><h3>"+floor_data.rooms[i].name+"</h3><p>Capactiy: "+floor_data.rooms[i].capacity+"</p></div>").appendTo($(this));
+										}
 									});
 								}).appendTo($(this));
 							}
-						}).appendTo($(this));
+						});
 					})
-				)
-				.append("<img src='http://placehold.it/250x150'>");
+				).append("<img src='http://placehold.it/250x150'>");
 		}
 	});
 });
