a95b2c0bdf48580eb4c7df81fb13b7f567773ec6
Alex Gaynor <alex.gaynor@gmail.com>
2011-11-06 17:17:35 -0500

    absolute path that works on all systems for hte db

--- rooms_project/settings.py
+++ rooms_project/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
     # local development settings for now
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/sdd/project/dev.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_ROOT, 'dev.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
